6ab0375ffeaa30fe973d270c2ee96a7e20655311
Zohaib Jamal <z1911j@gmail.com>
2024-09-24 22:59:57 +0500

    Jazzmin Error

--- eden-app/edenzapp/settings.py
+++ eden-app/edenzapp/settings.py
@@ -35,7 +35,6 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'http://92.113.2
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -151,4 +150,3 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
-
